04bbf2dad55dc1109daf8d31f02c6ac6b69cc4fa
Kris Selden <kris.selden@gmail.com>
2013-06-13 23:47:39 -0700

    fix tests

--- spec/models/card_manifest_spec.rb
+++ spec/models/card_manifest_spec.rb
@@ -4,7 +4,11 @@ describe CardManifest do
   describe "creation" do
     it "can be created" do
       lambda {
-        CardManifest.create!(name: "yapplabs/glazier", manifest: "{}")
+        CardManifest.create! do |card_manifest|
+          card_manifest.name = "yapplabs/glazier"
+          card_manifest.url = "http://something.cloudfront.com/card/yapplabs/glazier.json"
+          card_manifest.manifest = "{}"
+        end
       }.should change(CardManifest, :count).by(1)
     end
   end
