4499d1a40787b9c8f81853ea5dd0a1a07a82f76d
Kris Selden <kris.selden@gmail.com>
2013-06-14 00:50:51 -0700

    make ember data happy

--- app/serializers/dashboard_serializer.rb
+++ app/serializers/dashboard_serializer.rb
@@ -1,4 +1,4 @@
 class DashboardSerializer < ActiveModel::Serializer
   attribute :repository, key: :id
-  has_many :panes
+  has_many :panes, embed: :ids, include: true, key: :pane_ids, root: :panes
 end
