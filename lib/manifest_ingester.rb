7a1ccd45e06d3c690fc3bc64628a6943031dd04c
Stefan Penner <stefan.penner@gmail.com>
2013-06-14 16:01:05 -0400

    Fix card ingestation

--- lib/manifest_ingester.rb
+++ lib/manifest_ingester.rb
@@ -1,14 +1,27 @@
 require "net/http"
 require "uri"
+require "zlib"
 
 # provides manifest ingestation
 module ManifestIngester
-  def self.urlToJson(url)
+  extend self
+  IngestionFailedError = Class.new(StandardError)
+
+  def from_url(url)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     response = http.request_get(uri.path)
 
-    return unless (200...300).include? response.code
-    JSON.parse(github_response.body)
+    unless (200...300).include? response.code.to_i
+      raise IngestionFailedError, response
+    end
+
+    JSON.parse(inflate(response.body))
+  end
+
+private
+
+  def inflate(buffer)
+    Zlib::GzipReader.new(StringIO.new(buffer)).read
   end
 end
