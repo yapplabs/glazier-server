c78366ed415db0b9a644d59df5265e0ec4d745f3
Luke Melia <luke@lukemelia.com>
2013-06-19 13:18:57 -0400

    Add better error messaging when grunt ingest has not been run

--- app/models/page_template.rb
+++ app/models/page_template.rb
@@ -1,11 +1,17 @@
 class PageTemplate < ActiveRecord::Base
+  class NotFoundError < Error; end
+
   attr_accessible :key, :value
 
   def self.current(prefix)
     if Rails.env.test?
       "<html><head></head><body>Test #{prefix}</body></html>"
     else
-      self.find_by_key("#{prefix}:current").value
+      record = self.find_by_key("#{prefix}:current")
+      if record.nil?
+        raise NotFoundError.new("\"#{prefix}\" template not found. You may need to run `grunt ingest`.")
+      end
+      record.value
     end
   end
 end
