1f27d82babb16f612863d707a2a0c49a2d83b766
Kris Selden <kris.selden@gmail.com>
2013-07-09 16:33:35 -0700

    stub in code

--- app/controllers/pane_entries_controller.rb
+++ app/controllers/pane_entries_controller.rb
@@ -6,27 +6,27 @@ class PaneEntriesController < ApplicationController
 
     head :unauthorized unless current_user.has_dashboard?(pane.repository)
 
-    # PaneTypeUserEntry.transaction do
-    #   params[:data].each do |key, value|
-    #     PaneTypeUserEntry.add_or_update(
-    #       pane_type_name: params[:pane_type_name],
-    #       github_id: current_user[:github_id],
-    #       key: key,
-    #       value: value
-    #     )
-    #   end
-    # end
+    PaneEntry.transaction do
+      params[:data].each do |key, value|
+        PaneEntry.add_or_update(
+          pane_id: pane.id,
+          key: key,
+          value: value
+        )
+      end
+    end
+
+    head :no_content
   end
 
   def destroy
-    # card_entry = PaneTypeUserEntry.where(
-    #   pane_type_name: params[:pane_type_name],
-    #   github_id: current_user[:github_id],
-    #   key: params[:key]
-    # ).first
-    # if card_entry
-    #   card_entry.destroy
-    # end
-    # render json: {}
+    pane = Pane.find(params[:pane_id])
+
+    head :unauthorized unless current_user.has_dashboard?(pane.repository)
+
+    entry = PaneEntry.where(pane_id: pane.id, key: params[:key]).first
+    entry.destroy if entry
+
+    head :no_content
   end
 end
