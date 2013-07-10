58ea28ee6706a7d3c8ffcd4920819ea8be5b6649
Kris Selden <kris.selden@gmail.com>
2013-07-10 11:27:32 -0700

    show user's dashboards if logged in

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,23 @@
 class DashboardsController < ApplicationController
   def index
-    render json: Dashboard.where("repository in ('yapplabs/glazier','emberjs/ember.js')").all
+    if current_user
+      dashboards = current_user.dashboards
+    else
+      dashboards = default_dashboards
+    end
+    render json: dashboards
   end
 
   def show
     render json: Dashboard.find_or_bootstrap(params[:id])
+  end
+
+  private
+  def default_dashboards
+    Dashboard.where(
+      Dashboard.arel_table[:repository].in(
+        ['yapplabs/glazier', 'emberjs/ember.js']
+      )
+    ).all
   end
 end
