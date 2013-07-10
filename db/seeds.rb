d659b4314b24ac0649a6ca3de8840e1f78775e06
Kris Selden <kris.selden@gmail.com>
2013-07-10 15:52:23 -0700

    fix names

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 ActiveRecord::Base.transaction do
-
-  repositories_pane_type = PaneType.create do |pane_type|
-    pane_type.name = 'yapplabs/github-repositories'
-    pane_type.url = 'http://localhost:8000/cards/glazier-github-repositories/manifest.json'
-    pane_type.manifest = ActiveSupport::JSON.encode(
-      cardUrl: '/cards/glazier-github-repositories/card.js',
-      consumes: [ 'authenticatedGithubApi', 'repository', 'identity' ]
-    )
-  end
-
   issues_pane_type = PaneType.create do |pane_type|
-    pane_type.name = 'yapplabs/github-issues'
+    pane_type.name = 'glazier-github-issues'
     pane_type.url = 'http://localhost:8000/cards/glazier-github-issues/manifest.json'
     pane_type.manifest = ActiveSupport::JSON.encode(
       cardUrl: '/cards/glazier-github-issues/card.js',
@@ -27,8 +17,8 @@ ActiveRecord::Base.transaction do
   end
 
   stars_pane_type = PaneType.create do |pane_type|
-    pane_type.name = 'yapplabs/github-stars'
-    pane_type.url = 'http://localhost:8000/cards/github-stars/manifest.json'
+    pane_type.name = 'glazier-github-stars'
+    pane_type.url = 'http://localhost:8000/cards/glazier-github-stars/manifest.json'
     pane_type.manifest = ActiveSupport::JSON.encode(
       cardUrl: '/cards/glazier-github-stars/card.js',
       consumes: [ 'repository', 'unauthenticatedGithubApi', 'authenticatedGithubApi', 'identity' ]
@@ -37,10 +27,6 @@ ActiveRecord::Base.transaction do
 
   Dashboard.create do |dashboard|
     dashboard.repository = 'emberjs/ember.js'
-    dashboard.panes << Pane.create do |pane|
-      pane.id = '1eaa0cb9-45a6-4720-a3bb-f2f69c5602a2'
-      pane.pane_type = repositories_pane_type
-    end
 
     dashboard.panes << Pane.create do |pane|
       pane.id = 'c37b0ba4-cecc-11e2-8fa4-ef3e5db78e4d'
@@ -55,10 +41,6 @@ ActiveRecord::Base.transaction do
 
   Dashboard.create do |dashboard|
     dashboard.repository = 'yapplabs/glazier'
-    dashboard.panes << Pane.create do |pane|
-      pane.id = 'd30608af-11d8-402f-80a3-1f458650dbef'
-      pane.pane_type = repositories_pane_type
-    end
 
     dashboard.panes << Pane.create do |pane|
       pane.id = 'dca13978-cecc-11e2-b9e3-e342ecfc2ff7'
